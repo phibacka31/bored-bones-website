554b5f330e2c4e61dd8910c6d9be3a11cec0d31d
Philip Mueller <philipmueller@Philips-MacBook-Pro.local>
2025-07-20 20:11:26 -0500

    Make Book of Lore image smaller and responsive for mobile

--- app/lore/page.js
+++ app/lore/page.js
@@ -9,10 +9,24 @@ export default function LorePage() {
         <Image
           src="/images/LoreUpdate.png"
           alt="Lore Image"
-          width={700}
-          height={1100}
+          width={350}
+          height={550}
+          className="lore-image-responsive"
         />
       </div>
+      <style jsx>{`
+        .lore-image-responsive {
+          max-width: 350px;
+          width: 100%;
+          height: auto;
+        }
+        @media (max-width: 700px) {
+          .lore-image-responsive {
+            max-width: 180px;
+            width: 90vw;
+          }
+        }
+      `}</style>
       {/* End of Lore Image */}
     </main>
   );
